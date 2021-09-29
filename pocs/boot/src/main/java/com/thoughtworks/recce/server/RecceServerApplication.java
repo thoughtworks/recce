527e3731432b7a897b198e96664550a14d82e777
Chad Wilson <chadw@thoughtworks.com>
2021-09-29 16:35:50 +0800

    Add initial Spring Boot app template

---
+++ pocs/boot/src/main/java/com/thoughtworks/recce/server/RecceServerApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.recce.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecceServerApplication.class, args);
+	}
+
+}
