ef4a535183d867ee779407ec876205134a1e0227
Chad Wilson <chadw@thoughtworks.com>
2021-09-30 19:35:09 +0800

    Switch to using Exposed rather than Flyway to set up some test data
    Having something programmatic and DSL-based should be a lot easier to construct scenarios with, refactor etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:testcontainers")
     testImplementation("org.mockito:mockito-core")
+    testImplementation("org.jetbrains.exposed:exposed-core:0.34.1")
+    testImplementation("org.jetbrains.exposed:exposed-jdbc:0.34.1")
 
     testRuntimeOnly("com.h2database:h2")
 }
--- src/test/kotlin/com/thoughtworks/recce/server/DataSourceTest.kt
+++ src/test/kotlin/com/thoughtworks/recce/server/DataSourceTest.kt
@@ -1,36 +1,82 @@
 package com.thoughtworks.recce.server
 
-import io.micronaut.data.jdbc.runtime.JdbcOperations
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import jakarta.inject.Named
 import org.assertj.core.api.Assertions
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.sql.PreparedStatement
+import javax.sql.DataSource
 
 @MicronautTest
 class DataSourceTest {
 
     @Inject
     @field:Named("source")
-    lateinit var sourceOperations: JdbcOperations
+    lateinit var sourceDataSource: DataSource
 
     @Inject
     @field:Named("target")
-    lateinit var targetOperations: JdbcOperations
+    lateinit var targetDataSource: DataSource
+
+    object TestData : Table() {
+        val id = integer("id").autoIncrement()
+        val name = varchar("name", 255)
+        val value = varchar("value", 255)
+
+        override val primaryKey = PrimaryKey(id)
+    }
+
+    private val sourceDb: Database
+        get() {
+            return Database.connect(sourceDataSource)
+        }
+
+    private val targetDb: Database
+        get() {
+            return Database.connect(targetDataSource)
+        }
+
+    @BeforeEach
+    fun setup() {
+        for (db in listOf(sourceDb, targetDb)) {
+            transaction(db) {
+                SchemaUtils.create(TestData)
+
+                insertUsers(2)
+            }
+        }
+
+        transaction(sourceDb) {
+            insertUsers(1)
+        }
+        transaction(targetDb) {
+            insertUsers(2)
+        }
+    }
+
+    private fun insertUsers(num: Int) {
+        for (i in 0 until num) {
+            TestData.insert {
+                it[name] = "Test"
+                it[value] = "User"
+            }
+        }
+    }
 
     @Test
     fun `should load multiple data sources`() {
-        Assertions.assertThat(countRows(sourceOperations)).isEqualTo(3)
-        Assertions.assertThat(countRows(targetOperations)).isEqualTo(4)
-    }
-
-    private fun countRows(operations: JdbcOperations): Int? {
-        return operations.prepareStatement(
-            "select count(*) from testdata"
-        ) { preparedStatement: PreparedStatement ->
-            val resultSet = preparedStatement.executeQuery()
-            if (resultSet.first()) resultSet.getInt(1) else -1
+        transaction(sourceDb) {
+            Assertions.assertThat(TestData.selectAll().count()).isEqualTo(3)
+        }
+        transaction(targetDb) {
+            Assertions.assertThat(TestData.selectAll().count()).isEqualTo(4)
         }
     }
 }
