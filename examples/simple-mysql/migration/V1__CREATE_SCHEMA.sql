56dc67eb8abdca7cc4d872015b661987635d400a
Chad Wilson <chadw@thoughtworks.com>
2021-10-17 16:22:54 +0800

    #17 Change sample database schema to have column types the code currently understands how to map for simplicity

--- examples/simple-mysql/migration/V1__CREATE_SCHEMA.sql
+++ examples/simple-mysql/migration/V1__CREATE_SCHEMA.sql
@@ -2,9 +2,8 @@ CREATE TABLE data_set_migration_run
 (
     id             INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     data_set_id    VARCHAR(255)                       NOT NULL,
-    created_time   TIMESTAMP                          NOT NULL,
-    updated_time   TIMESTAMP                          NOT NULL,
-    completed_time TIMESTAMP
+    source_records INTEGER,
+    target_records INTEGER
 );
 
 CREATE TABLE data_set_migration_record
@@ -17,8 +16,8 @@ CREATE TABLE data_set_migration_record
     FOREIGN KEY (migration_id) REFERENCES data_set_migration_run (id)
 );
 
-INSERT INTO data_set_migration_run (data_set_id, created_time, updated_time, completed_time)
-VALUES ('test-dataset', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO data_set_migration_run (data_set_id, source_records, target_records)
+VALUES ('test-dataset', 1234, 5678);
 
 INSERT INTO data_set_migration_record (migration_id, migration_key, source_data, target_data)
 VALUES (1, 'Key', 'SourceHash', 'TargetHash');
