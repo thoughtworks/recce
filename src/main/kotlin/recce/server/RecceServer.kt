0fd75a1f2fd421053cee964037874e47a2257193
Chad Wilson <chadw@thoughtworks.com>
2021-11-08 17:02:09 +0800

    Disable the Micronaut banner

--- src/main/kotlin/recce/server/RecceServer.kt
+++ src/main/kotlin/recce/server/RecceServer.kt
@@ -4,6 +4,7 @@ import io.micronaut.runtime.Micronaut.build
 
 fun main(args: Array<String>) {
     build()
+        .banner(false)
         .args(*args)
         .packages("recce.server")
         .start()
