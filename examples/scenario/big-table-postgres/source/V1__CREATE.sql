689fe161f8b95597695c4eee2444f7f2e6b1f2e3
Chad Wilson <chadw@thoughtworks.com>
2021-11-16 10:50:41 +0800

    Modify "big postgres" scenario to allow combinations of different row match semantics
    Allows running scenarios where 50,000 rows are in source only, 50,000 in target only, 50,000 in both only, and the combination (150,000 rows). This allows testing the batching performance a little better.

--- examples/scenario/big-table-postgres/source/V1__CREATE.sql
+++ examples/scenario/big-table-postgres/source/V1__CREATE.sql
@@ -1,15 +1,16 @@
 CREATE TABLE big_table
 (
-    id             SERIAL PRIMARY KEY NOT NULL,
+    id             INTEGER PRIMARY KEY NOT NULL,
     category       VARCHAR(100) NOT NULL,
     name           VARCHAR(255) NOT NULL UNIQUE,
     status         VARCHAR(20) NOT NULL
 );
 
 INSERT INTO big_table (
-    category, name, status
+    id, category, name, status
 )
 SELECT
+    i,
     md5(random()::text),
     md5(random()::text),
     left(md5(random()::text), 4)
--- examples/scenario/big-table-postgres/target/V1__CREATE.sql
+++ examples/scenario/big-table-postgres/target/V1__CREATE.sql
@@ -1,16 +1,17 @@
 CREATE TABLE big_table
 (
-    id             SERIAL PRIMARY KEY NOT NULL,
+    id             INTEGER PRIMARY KEY NOT NULL,
     category       VARCHAR(100) NOT NULL,
     name           VARCHAR(255) NOT NULL UNIQUE,
     status         VARCHAR(20) NOT NULL
 );
 
 INSERT INTO big_table (
-    category, name, status
+    id, category, name, status
 )
 SELECT
+    i,
     md5(random()::text),
     md5(random()::text),
     left(md5(random()::text), 20)
-from generate_series(1, 100000) s(i);
+from generate_series(50001, 150000) s(i);
