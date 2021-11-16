69f36d9a8ffd98923134053025bd3fe175e51d1e
Chad Wilson <chadw@thoughtworks.com>
2021-11-16 10:58:06 +0800

    #40 First attempt to batch the target inserts
    - divides each target row into batches
    - batch searches for whether they already exist from source
    - if they do, updates them one-by-one (haven't done bulk updates with Micronaut Data just yet)
    - any remaining which were not found+updated are batch inserted
    
    Still to resolve
    - large batch sizes cause things to lock up and halt - some kind of exhaustion, probably of connections
    - this leaves the slowest bit the updates to existing rows which are done one-by-one

--- src/main/kotlin/recce/server/dataset/DatasetRecService.kt
+++ src/main/kotlin/recce/server/dataset/DatasetRecService.kt
@@ -3,6 +3,7 @@ package recce.server.dataset
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import mu.KotlinLogging
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.util.function.component1
 import reactor.kotlin.core.util.function.component2
@@ -64,27 +65,50 @@ open class DatasetRecService(
             .map { it.invoke() }
             .doOnNext { logger.info { "Load from source completed" } }
 
-    private fun loadFromTarget(target: DataLoadDefinition, run: Mono<RecRun>): Mono<DatasetMeta> =
-        target.runQuery()
+    private fun loadFromTarget(target: DataLoadDefinition, run: Mono<RecRun>): Mono<DatasetMeta> {
+        val bufferedRows = target.runQuery()
             .doOnNext { logger.info { "Target query completed; streaming to Recce DB" } }
             .flatMap { result -> result.map(HashedRow::fromRow) }
+            .buffer(config.defaultBatchSize)
             .zipWith(run.repeat())
-            .flatMap { (row, run) ->
-                recordRepository
-                    .existsByRecRunIdAndMigrationKey(run.id!!, row.migrationKey)
-                    .flatMap { exists ->
-                        when (exists) {
-                            true -> recordRepository.updateByRecRunIdAndMigrationKey(run.id, row.migrationKey, targetData = row.hashedValue).thenReturn(row)
-                            false -> recordRepository.save(RecRecord(recRunId = run.id, migrationKey = row.migrationKey, targetData = row.hashedValue))
-                        }
+        val batches: Flux<() -> DatasetMeta> = bufferedRows
+            .flatMap { (rows, run) ->
+//                logger.info { "Processing batch of size [${rows.size}] from target"}
+                val toPersist = rows.associateByTo(mutableMapOf()) { it.migrationKey }
+                val updatedRows = recordRepository
+                    .findByRecRunIdAndMigrationKeyIn(run.id!!, rows.map { it.migrationKey })
+                    .flatMap { found ->
+                        recordRepository.updateByRecRunIdAndMigrationKey(
+                            found.recRunId,
+                            found.migrationKey,
+                            targetData = toPersist.remove(found.migrationKey)?.hashedValue
+                        ).then(Mono.just(found))
                     }
-                    .then(Mono.just(row.lazyMeta()))
+                val newRows = Flux
+                    .defer {
+//                        logger.info { "Checking new rows for batch of size [${toPersist.size}]" }
+                        if (toPersist.isEmpty()) Mono.empty() else Mono.just(toPersist.values)
+                    }
+                    .map { hashedRows ->
+                        hashedRows.map {
+                            RecRecord(
+                                recRunId = run.id,
+                                migrationKey = it.migrationKey,
+                                targetData = it.hashedValue
+                            )
+                        }
+                    }.flatMap { recordRepository.saveAll(it) }
+
+                updatedRows.concatWith(newRows).map { rows.first().lazyMeta() }
             }
+        return batches
+            .doOnNext { logger.debug { "$it emitted" } }
             .onErrorMap { DataLoadException("Failed to load data from target [${target.dataSourceRef}]: ${it.message}", it) }
             .defaultIfEmpty { DatasetMeta() }
             .last()
             .map { it.invoke() }
             .doOnNext { logger.info { "Load from target completed" } }
+    }
 }
 
 class DataLoadException(message: String, cause: Throwable) : Exception(message, cause)
