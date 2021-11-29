28973bcfaf3f52cc86d34821bd3d54adec15ee1c
Chad Wilson <chadw@thoughtworks.com>
2021-11-29 21:29:11 +0800

    #39 Run actual reconciliations between all combinations of database types
    Rather than running test/dummy connectivity queries with R2DBC, run actual reconciliations here

--- src/test/kotlin/recce/server/dataset/datasource/MultiDataSourceConnectivityIntegrationTest.kt
+++ src/test/kotlin/recce/server/dataset/datasource/MultiDataSourceConnectivityIntegrationTest.kt
@@ -2,8 +2,7 @@ package recce.server.dataset.datasource
 
 import com.google.common.collect.Sets
 import io.micronaut.context.ApplicationContext
-import io.micronaut.data.r2dbc.operations.R2dbcOperations
-import io.micronaut.inject.qualifiers.Qualifiers
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
@@ -24,8 +23,9 @@ import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import recce.server.dataset.DatasetRecService
+import recce.server.recrun.MatchStatus
 import java.util.stream.Stream
 
 @Testcontainers
@@ -51,35 +51,48 @@ internal open class MultiDataSourceConnectivityIntegrationTest {
             "mssql" to mssql
         )
 
+        @Suppress("UnstableApiUsage")
+        private val databaseCombinations = Sets.combinations(databases.keys, 2)
+
         private lateinit var ctx: ApplicationContext
 
         @JvmStatic
         @BeforeAll
-        fun setupOnce() {
-            ctx = ApplicationContext.run(
-                databases.flatMap { (name, container) ->
-                    listOf(
-                        "r2dbc.datasources.$name.url" to container.jdbcUrl.replace("jdbc", "r2dbc:pool"),
-                        "r2dbc.datasources.$name.username" to container.username,
-                        "r2dbc.datasources.$name.password" to container.password
-                    )
-                }.toMap(),
-                "test"
-            )
+        fun startApplication() {
+            val datasources = databases.flatMap { (name, container) ->
+                listOf(
+                    "r2dbc.datasources.$name.url" to container.jdbcUrl.replace("jdbc", "r2dbc:pool"),
+                    "r2dbc.datasources.$name.username" to container.username,
+                    "r2dbc.datasources.$name.password" to container.password
+                )
+            }.toMap()
+
+            val datasets = databaseCombinations.map { Pair(it.first(), it.last()) }.flatMap { (source, target) ->
+                listOf(
+                    "reconciliation.datasets.$source-to-$target.source.dataSourceRef" to source,
+                    "reconciliation.datasets.$source-to-$target.source.query" to "SELECT id as MigrationKey, name, value FROM TestData",
+                    "reconciliation.datasets.$source-to-$target.target.dataSourceRef" to target,
+                    "reconciliation.datasets.$source-to-$target.target.query" to "SELECT id as MigrationKey, name, value FROM TestData",
+                )
+            }.toMap()
+
+            ctx = ApplicationContext.run(datasources + datasets)
         }
 
         @JvmStatic
         @AfterAll
-        fun tearDownOnce() {
+        fun stopApplication() {
             ctx.stop()
         }
 
         private fun connectToJdbc(container: JdbcDatabaseContainer<Nothing>): Database {
             return Database.connect(container.jdbcUrl, user = container.username, password = container.password)
         }
+    }
 
-        private fun connectToReactive(database: String) =
-            ctx.getBean(R2dbcOperations::class.java, Qualifiers.byName(database)).connectionFactory().create()
+    @AfterEach
+    fun `wipe databases`() {
+        databases.forEach { transaction(connectToJdbc(it.value)) { SchemaUtils.drop(TestData) } }
     }
 
     protected object TestData : Table() {
@@ -109,43 +122,37 @@ internal open class MultiDataSourceConnectivityIntegrationTest {
     class DatabaseCombinations : ArgumentsProvider {
         @Suppress("UnstableApiUsage")
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
-            return Sets.combinations(databases.keys, 2)
-                .map { Arguments.of(*it.toTypedArray()) }.stream()
+            return databaseCombinations.map { Arguments.of(*it.toTypedArray()) }.stream()
         }
-    }
-
-    @AfterEach
-    fun `wipe databases`() {
-        databases.forEach { transaction(connectToJdbc(it.value)) { SchemaUtils.drop(TestData) } }
     }
 
     @ParameterizedTest
     @ArgumentsSource(DatabaseCombinations::class)
-    fun `should run rec between source and target`(sourceDatabase: String, targetDatabase: String) {
+    fun `should run rec between source and target`(source: String, target: String) {
 
         createTestData(
             connectToJdbc(
-                databases[sourceDatabase] ?: throw IllegalArgumentException("Cannot find db type [$sourceDatabase].")
+                databases[source] ?: throw IllegalArgumentException("Cannot find db type [$source].")
             )
         )
 
         createTestData(
             connectToJdbc(
-                databases[targetDatabase] ?: throw IllegalArgumentException("Cannot find db type [$targetDatabase].")
+                databases[target] ?: throw IllegalArgumentException("Cannot find db type [$target].")
             )
         )
 
-        StepVerifier.create(getCount(sourceDatabase))
-            .expectNext(4)
-            .verifyComplete()
-
-        StepVerifier.create(getCount(targetDatabase))
-            .expectNext(4)
+        StepVerifier.create(ctx.getBean(DatasetRecService::class.java).runFor("$source-to-$target"))
+            .assertNext { run ->
+                assertThat(run.summary).usingRecursiveComparison().isEqualTo(
+                    MatchStatus(
+                        sourceOnly = 0,
+                        targetOnly = 0,
+                        bothMatched = 4,
+                        bothMismatched = 0
+                    )
+                )
+            }
             .verifyComplete()
     }
-
-    private fun getCount(database: String) = Mono.from(connectToReactive(database))
-        .flatMapMany { it.createStatement("SELECT count(*) as count from TestData").execute() }
-        .flatMap { result -> result.map { row, _ -> row.get("count") as Number } }
-        .map { it.toLong() }
 }
