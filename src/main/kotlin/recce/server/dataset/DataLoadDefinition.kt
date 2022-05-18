31c2dac58b0e8701eec0c0d1b173f5d201669f11
jordan396-tw <jordan.tan@thoughtworks.com>
2022-05-18 16:48:01 +0800

    #158 add queryFile property in micronaut config
    - add tests for method to resolve query statement
    - add method to resolve query statement
    - set query parameter as optional

--- src/main/kotlin/recce/server/dataset/DataLoadDefinition.kt
+++ src/main/kotlin/recce/server/dataset/DataLoadDefinition.kt
@@ -1,21 +1,43 @@
 package recce.server.dataset
 
 import io.micronaut.context.BeanLocator
+import io.micronaut.context.annotation.ConfigurationInject
 import io.micronaut.context.exceptions.ConfigurationException
 import io.micronaut.data.r2dbc.operations.R2dbcOperations
 import io.micronaut.inject.qualifiers.Qualifiers
 import io.r2dbc.spi.Result
 import reactor.core.publisher.Flux
 import recce.server.PostConstructable
+import java.io.File
 import javax.validation.constraints.NotBlank
 
-class DataLoadDefinition(@NotBlank val datasourceRef: String, @NotBlank val query: String) : PostConstructable {
+class DataLoadDefinition
+@ConfigurationInject constructor(
+    @NotBlank val datasourceRef: String,
+    val query: String = "",
+    private val queryFile: String = ""
+) : PostConstructable {
     lateinit var dbOperations: R2dbcOperations
     lateinit var role: DataLoadRole
+    lateinit var queryStatement: String
 
     override fun populate(locator: BeanLocator) {
         dbOperations = locator.findBean(R2dbcOperations::class.java, Qualifiers.byName(datasourceRef))
             .orElseThrow { ConfigurationException("Cannot locate ${R2dbcOperations::class.java.simpleName} named [$datasourceRef] in configuration!") }
+
+        queryStatement = resolveQueryStatement()
+    }
+
+    private fun resolveQueryStatement(): String {
+        return query.ifEmpty {
+            if (queryFile.isEmpty())
+                throw ConfigurationException("Either query or queryFile must be provided!")
+            try {
+                File(queryFile).readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                throw ConfigurationException("Cannot load query statement from queryFile $queryFile: ${e.message}")
+            }
+        }
     }
 
     companion object {
--- src/test/kotlin/recce/server/dataset/DataLoadDefinitionTest.kt
+++ src/test/kotlin/recce/server/dataset/DataLoadDefinitionTest.kt
@@ -15,12 +15,24 @@ import org.mockito.Answers
 import org.mockito.kotlin.*
 import reactor.core.publisher.Mono
 import reactor.kotlin.test.test
+import java.net.URL
+import java.nio.file.Paths
 import java.util.*
 
 internal class DataLoadDefinitionTest {
     private val testSourceName = "source1"
     private val testQuery = "SELECT * FROM somewhere"
-    private lateinit var definition: DataLoadDefinition
+    private val testQueryStatementFromFile = "SELECT * FROM elsewhere\n"
+    private val res: URL = javaClass.classLoader.getResource("config/test-query.sql")
+    private val testQueryFile = Paths.get(res.toURI()).toFile().absolutePath
+    private val testQueryInvalidFile = "test-invalid-query.sql"
+
+    private lateinit var definitionQuery: DataLoadDefinition
+    private lateinit var definitionQueryFromFile: DataLoadDefinition
+    private lateinit var definitionQueryFromInvalidFile: DataLoadDefinition
+    private lateinit var definitionQueryAndQueryFromFile: DataLoadDefinition
+    private lateinit var definitionQueryAndQueryFromInvalidFile: DataLoadDefinition
+    private lateinit var definitionNoQueryAndNoQueryFromFile: DataLoadDefinition
 
     private val mockConnection: Connection = mock {
         on { close() } doReturn Mono.empty()
@@ -28,7 +40,72 @@ internal class DataLoadDefinitionTest {
 
     @BeforeEach
     fun setUp() {
-        definition = DataLoadDefinition(testSourceName, testQuery).apply { role = DataLoadRole.Source }
+        definitionQuery = DataLoadDefinition(testSourceName, testQuery).apply { role = DataLoadRole.Source }
+        definitionQueryFromFile = DataLoadDefinition(testSourceName, "", testQueryFile).apply { role = DataLoadRole.Source }
+        definitionQueryFromInvalidFile = DataLoadDefinition(testSourceName, "", testQueryInvalidFile).apply { role = DataLoadRole.Source }
+        definitionQueryAndQueryFromFile = DataLoadDefinition(testSourceName, testQuery, testQueryFile).apply { role = DataLoadRole.Source }
+        definitionQueryAndQueryFromInvalidFile = DataLoadDefinition(testSourceName, testQuery, testQueryInvalidFile).apply { role = DataLoadRole.Source }
+        definitionNoQueryAndNoQueryFromFile = DataLoadDefinition(testSourceName, "", "").apply { role = DataLoadRole.Source }
+    }
+
+    @Test
+    fun `should load query statement from file if valid query file provided`() {
+        val operations = mock<R2dbcOperations>()
+        val beanLocator = mock<BeanLocator> {
+            on { findBean(any<Class<Any>>(), eq(Qualifiers.byName(testSourceName))) } doReturn Optional.of(operations)
+        }
+
+        definitionQueryFromFile.populate(beanLocator)
+
+        assertThat(definitionQueryFromFile.queryStatement).isEqualTo(testQueryStatementFromFile)
+    }
+
+    @Test
+    fun `should fail to load query statement from file if invalid query file provided`() {
+        val operations = mock<R2dbcOperations>()
+        val beanLocator = mock<BeanLocator> {
+            on { findBean(any<Class<Any>>(), eq(Qualifiers.byName(testSourceName))) } doReturn Optional.of(operations)
+        }
+
+        assertThatThrownBy { definitionQueryFromInvalidFile.populate(beanLocator) }
+            .isExactlyInstanceOf(ConfigurationException::class.java)
+            .hasMessageContaining("Cannot load query statement from queryFile")
+    }
+
+    @Test
+    fun `should load query statement from query if both query and query file provided`() {
+        val operations = mock<R2dbcOperations>()
+        val beanLocator = mock<BeanLocator> {
+            on { findBean(any<Class<Any>>(), eq(Qualifiers.byName(testSourceName))) } doReturn Optional.of(operations)
+        }
+
+        definitionQueryAndQueryFromFile.populate(beanLocator)
+
+        assertThat(definitionQueryAndQueryFromFile.queryStatement).isEqualTo(testQuery)
+    }
+
+    @Test
+    fun `should load query statement from query if both query and invalid query file provided`() {
+        val operations = mock<R2dbcOperations>()
+        val beanLocator = mock<BeanLocator> {
+            on { findBean(any<Class<Any>>(), eq(Qualifiers.byName(testSourceName))) } doReturn Optional.of(operations)
+        }
+
+        definitionQueryAndQueryFromInvalidFile.populate(beanLocator)
+
+        assertThat(definitionQueryAndQueryFromInvalidFile.queryStatement).isEqualTo(testQuery)
+    }
+
+    @Test
+    fun `should fail to load query statement from query if query not provided and query file not provided`() {
+        val operations = mock<R2dbcOperations>()
+        val beanLocator = mock<BeanLocator> {
+            on { findBean(any<Class<Any>>(), eq(Qualifiers.byName(testSourceName))) } doReturn Optional.of(operations)
+        }
+
+        assertThatThrownBy { definitionNoQueryAndNoQueryFromFile.populate(beanLocator) }
+            .isExactlyInstanceOf(ConfigurationException::class.java)
+            .hasMessageContaining("Either query or queryFile must be provided!")
     }
 
     @Test
@@ -38,20 +115,20 @@ internal class DataLoadDefinitionTest {
             on { findBean(any<Class<Any>>(), eq(Qualifiers.byName(testSourceName))) } doReturn Optional.of(operations)
         }
 
-        definition.populate(beanLocator)
+        definitionQuery.populate(beanLocator)
 
-        assertThat(definition.dbOperations).isEqualTo(operations)
+        assertThat(definitionQuery.dbOperations).isEqualTo(operations)
     }
 
     @Test
     fun `should produce short descriptor with role`() {
-        assertThat(definition.datasourceDescriptor).isEqualTo("Source(ref=source1)")
+        assertThat(definitionQuery.datasourceDescriptor).isEqualTo("Source(ref=source1)")
     }
 
     @Test
     fun `should throw on failure to find bean`() {
 
-        assertThatThrownBy { definition.populate(mock()) }
+        assertThatThrownBy { definitionQuery.populate(mock()) }
             .isExactlyInstanceOf(ConfigurationException::class.java)
             .hasMessageContaining("source1")
     }
@@ -65,13 +142,13 @@ internal class DataLoadDefinitionTest {
             on { execute() } doReturn Mono.just(result)
         }
 
-        definition.dbOperations = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS) {
+        definitionQuery.dbOperations = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS) {
             on { connectionFactory().create() } doReturn Mono.just(mockConnection)
         }
 
         whenever(mockConnection.createStatement(eq(testQuery))).thenReturn(statement)
 
-        definition.runQuery()
+        definitionQuery.runQuery()
             .test()
             .expectNext(result)
             .verifyComplete()
@@ -85,11 +162,11 @@ internal class DataLoadDefinitionTest {
 
     @Test
     fun `should close connection after failed query`() {
-        definition.dbOperations = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS) {
+        definitionQuery.dbOperations = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS) {
             on { connectionFactory().create() } doReturn Mono.just(mockConnection)
         }
 
-        definition.runQuery()
+        definitionQuery.runQuery()
             .test()
             .expectErrorSatisfies {
                 assertThat(it)
