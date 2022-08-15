bee8cb86577fc9ae29ad2b969595fab4d76b99b7
Chad Wilson <chadw@thoughtworks.com>
2022-08-15 18:48:54 +0800

    Bump Kotlin from 1.7.0 to 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("GradlePackageUpdate")
 
 plugins {
-    val kotlinVersion = "1.7.0"
+    val kotlinVersion = "1.7.10"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.kapt") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
