3563f9b036be9d4c1f45f7c984559b295383d841
Chad Wilson <chadw@thoughtworks.com>
2023-01-07 12:48:02 +0800

    Bump owasp-dependency-check from 7.4.3 to 7.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("com.adarshr.test-logger") version "3.2.0"
     id("com.google.cloud.tools.jib") version "3.3.1"
     id("org.ajoberstar.reckon") version "0.16.1"
-    id("org.owasp.dependencycheck") version "7.4.3"
+    id("org.owasp.dependencycheck") version "7.4.4"
 }
 
 group = "recce.server"
