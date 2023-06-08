b9e6b0d3c8f1946a8aba71b6495b7f78f0b16964
Chad Wilson <chadw@thoughtworks.com>
2023-06-08 16:00:08 +0800

    Bump batect to 0.83.8

--- batect.cmd
+++ batect.cmd
@@ -6,7 +6,7 @@ rem For more information, visit https://github.com/batect/batect.
 
 setlocal EnableDelayedExpansion
 
-set "version=0.83.6"
+set "version=0.83.8"
 
 if "%BATECT_CACHE_DIR%" == "" (
     set "BATECT_CACHE_DIR=%USERPROFILE%\.batect\cache"
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'^
 
 ^
 
-$Version='0.83.6'^
+$Version='0.83.8'^
 
 ^
 
@@ -48,7 +48,7 @@ $UrlEncodedVersion = [Uri]::EscapeDataString($Version)^
 
 $DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/batect-$UrlEncodedVersion.jar"^
 
-$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM 'd31a2931e96d2df1cde64e0948ffb57ce818b380b688ccc31f17617a64a3b32c'^
+$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '5bfbd8063b21ca899b0da8c951fccd022d068c750c8828ac98149d0508c07856'^
 
 ^
 
