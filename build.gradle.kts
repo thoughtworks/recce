a224eee948f84d245cc6ae817c2f233ed4f0b627
Chad Wilson <chadw@thoughtworks.com>
2023-12-18 21:41:36 +0800

    Bump expected reactor tools version

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ val depVersions =
     depDescriptors.mapValues { (_, v) -> v.split(':').last() } +
         mapOf(
             "javaMajor" to "17",
-            "reactorToolsVersionExpected" to "3.6.0"
+            "reactorToolsVersionExpected" to "3.6.1"
         )
 
 repositories {
