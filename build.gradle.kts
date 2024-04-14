1c04e42d107f0a2faaa447c446d41c749da3ffdd
Chad Wilson <chadw@thoughtworks.com>
2024-04-15 00:46:11 +0800

    Correct expected reactor tools version

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ val depVersions =
     depDescriptors.mapValues { (_, v) -> v.split(':').last() } +
         mapOf(
             "javaMajor" to "17",
-            "reactorToolsVersionExpected" to "3.6.3"
+            "reactorToolsVersionExpected" to "3.6.5"
         )
 
 repositories {
