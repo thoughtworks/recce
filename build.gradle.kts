e8054e968fb2a7d1e0bab69f2dc0360946e0ec3a
Chad Wilson <chadw@thoughtworks.com>
2024-06-26 13:03:40 +0800

    Bump kotlin to 1.9.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("GradlePackageUpdate")
 
 plugins {
-    val kotlinVersion = "1.9.23"
+    val kotlinVersion = "1.9.25"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.kapt") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
