658687d97efe33e016128836792d627a68584e64
Chad Wilson <chadw@thoughtworks.com>
2024-08-23 13:51:56 +0800

    Make basic auth constant-time

--- src/main/kotlin/recce/server/auth/BasicAuthenticationProvider.kt
+++ src/main/kotlin/recce/server/auth/BasicAuthenticationProvider.kt
@@ -9,6 +9,7 @@ import jakarta.inject.Singleton
 import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
+import java.security.MessageDigest
 
 @Singleton
 class BasicAuthenticationProvider(private val authConfiguration: AuthConfiguration) : AuthenticationProvider {
@@ -18,8 +19,8 @@ class BasicAuthenticationProvider(private val authConfiguration: AuthConfigurati
     ): Publisher<AuthenticationResponse> =
         Flux.create({ emitter: FluxSink<AuthenticationResponse> ->
             if (
-                authenticationRequest.identity == authConfiguration.username &&
-                authenticationRequest.secret == authConfiguration.password
+                authConfiguration.username.constantTimeEquals(authenticationRequest.identity) &&
+                authConfiguration.password.constantTimeEquals(authenticationRequest.secret)
             ) {
                 emitter.next(AuthenticationResponse.success(authenticationRequest.identity as String))
                 emitter.complete()
@@ -27,4 +28,11 @@ class BasicAuthenticationProvider(private val authConfiguration: AuthConfigurati
                 emitter.error(AuthenticationResponse.exception(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH))
             }
         }, FluxSink.OverflowStrategy.ERROR)
+}
+
+fun String?.constantTimeEquals(other: Any?): Boolean {
+    if (this == null || other !is String) {
+        return this == other
+    }
+    return MessageDigest.isEqual(this.toByteArray(), other.toByteArray())
 }
